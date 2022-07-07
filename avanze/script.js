31c2c450fad7cc18448455227aad0b5e5f3bb5cf
Nooruddin Saheb <44988545+nrnsaheb@users.noreply.github.com>
2022-07-07 18:15:58 +0530

    Add files via upload

---
+++ avanze/script.js
@@ -0,0 +1,37 @@
+$(function () {
+    $('a[href="#search"]').on('click', function(event) {
+        event.preventDefault();
+        $('#search').addClass('open');
+        $('#search > form > input[type="search"]').focus();
+    });
+    
+    $('#search, #search button.close').on('click keyup', function(event) {
+        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+            $(this).removeClass('open');
+        }
+    });
+    
+    
+    //Do not include! This prevents the form from submitting for DEMO purposes only!
+    $('form').submit(function(event) {
+        event.preventDefault();
+        return false;
+    })
+});
+$(document).ready(function() {
+    // executes when HTML-Document is loaded and DOM is ready
+   console.log("document is ready");
+     
+   
+     $( ".card" ).hover(
+     function() {
+       $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
+     }, function() {
+       $(this).removeClass('shadow-lg');
+     }
+   );
+     
+   // document ready  
+   });
+   
+   
